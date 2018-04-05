fc67751076b908f8c71b5209ee075efde5f4e54d
Gabriel Flores <gabriel330flores@gmail.com>
2018-04-05 02:53:52 -0500

    Added a bunch of nonsense

---
+++ script.js
@@ -0,0 +1,23 @@
+let score = 0;
+let display = "Your score is " + score + " out of 5";
+
+class Correct{
+    constructor(ca1){
+        this.ca1 = document.getElementsById('correct1');
+        let answer = this.ca1;        
+    }
+}
+
+function calcScore(){
+    if(answer = this.ca1){
+        return score + 1;
+    }
+    else
+    {
+        return score;
+    }
+    document.getElementById('score').innerHTML + display;
+}
+
+
+
