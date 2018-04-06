25f28df149ecb9e0ff69b64e180571d0be027476
Gabriel Flores <gabriel330flores@gmail.com>
2018-04-06 16:26:48 -0500

    Added two more questions

--- script.js
+++ script.js
@@ -24,6 +24,20 @@ function submitAnswers(){
     else if(document.getElementById('result3')){
         document.getElementById('result3').innerHTML = "Incorrect!";
     }
+
+    if(document.getElementById('correct4').checked){
+        document.getElementById('result4').innerHTML = "Correct!";
+    }
+    else if(document.getElementById('result4')){
+        document.getElementById('result4').innerHTML = "Incorrect!";
+    }
+
+    if(document.getElementById('correct5').checked){
+        document.getElementById('result5').innerHTML = "Correct!";
+    }
+    else if(document.getElementById('result5')){
+        document.getElementById('result5').innerHTML = "Incorrect!";
+    }
 }
 
 
