71cffa72bd6bc0e0f80be3af9685106ffe00add6
Gabriel Flores <gabriel330flores@gmail.com>
2018-04-06 15:30:10 -0500

    Added question functionality

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 let score = 0;
 let display = "Your score is " + score + " out of 5";
 
-class Correct{
-    constructor(ca1){
-        this.ca1 = document.getElementsById('correct1');
-        let answer = this.ca1;        
-    }
-}
+// class Correct{
+//     constructor(ca1){
+//         this.ca1 = document.getElementsById('correct1');
+//         let answer = this.ca1;        
+//     }
+// }
+
+// function calcScore(){
+//     if(answer = this.ca1){
+//         return score + 1;
+//     }
+//     else
+//     {
+//         return score;
+//     }
+//     document.getElementById('score').innerHTML + display;
+// }
+
 
 function calcScore(){
-    if(answer = this.ca1){
-        return score + 1;
+   
+
+    if(document.getElementById('correct1').checked){
+        document.getElementById('score').innerHTML = "Correct!";
     }
-    else
-    {
-        return score;
+    else if(document.getElementById('score')){
+        document.getElementById('score').innerHTML = "Incorrect!";
     }
-    document.getElementById('score').innerHTML + display;
 }
 
 
