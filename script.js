cfa688cfad1b42bd2cb46ae8b388d9eb27ea707a
Gabriel Flores <gabriel330flores@gmail.com>
2018-04-06 16:06:53 -0500

    Added a second question

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 let score = 0;
 let display = "Your score is " + score + " out of 5";
 
-// class Correct{
-//     constructor(ca1){
-//         this.ca1 = document.getElementsById('correct1');
-//         let answer = this.ca1;        
-//     }
-// }
-
-// function calcScore(){
-//     if(answer = this.ca1){
-//         return score + 1;
-//     }
-//     else
-//     {
-//         return score;
-//     }
-//     document.getElementById('score').innerHTML + display;
-// }
-
-
-function calcScore(){
+function submitAnswers(){
    
 
     if(document.getElementById('correct1').checked){
-        document.getElementById('score').innerHTML = "Correct!";
+        document.getElementById('result1').innerHTML = "Correct!";
     }
-    else if(document.getElementById('score')){
-        document.getElementById('score').innerHTML = "Incorrect!";
+    else if(document.getElementById('result1')){
+        document.getElementById('result1').innerHTML = "Incorrect!";
+    }
+
+    if(document.getElementById('correct2').checked){
+        document.getElementById('result2').innerHTML = "Correct!";
+    }
+    else if(document.getElementById('result2')){
+        document.getElementById('result2').innerHTML = "Incorrect!";
     }
 }
 
